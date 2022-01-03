ec038b0b4656ce3a1c4868e293662cb5e785ee4d
khan-osama <osamaa.khan61@gmail.com>
2022-01-03 14:24:47 -0800

    Moved to check length before for loop

--- array-manipulation-3/src/equal.js
+++ array-manipulation-3/src/equal.js
@@ -1,8 +1,12 @@
 /* exported equal */
 function equal(first, second) {
-  for (let i = 0; i < first.length; i++) {
-    if (first[i] !== second[i] || first.length !== second.length) {
-      return false;
+  if (first.length !== second.length) {
+    return false;
+  } else {
+    for (let i = 0; i < first.length; i++) {
+      if (first[i] !== second[i]) {
+        return false;
+      }
     }
   }
   return true;
