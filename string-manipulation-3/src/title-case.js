aa78b3190773a0fe13ef561bf961569c3288b8a1
khan-osama <osamaa.khan61@gmail.com>
2022-01-03 14:21:51 -0800

    Updated and added some words to omit and will capitalize all first words in the title

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -5,7 +5,7 @@ function titleCase(title) {
   let temp = null;
   for (let i = 0; i < title.length; i++) {
     temp = title[i];
-    if (temp.length > 3 || title[i].charAt(0) === 'w') {
+    if ((temp.length > 2 && title[i] !== 'for' && title[i] !== 'the' && title[i] !== 'and' && title[i] !== 'but') || i === 0) {
       temp = title[i].charAt(0).toUpperCase() + title[i].slice(1);
       if (temp.includes('-')) {
         const [firstWord, secondWord] = temp.split('-');
@@ -19,7 +19,7 @@ function titleCase(title) {
     } else if (temp.length <= 3 && title[i - 1].includes(':')) {
       temp = title[i].charAt(0).toUpperCase() + title[i].slice(1);
     }
-    if (temp === 'api') {
+    if (temp === 'Api') {
       temp = 'API';
     }
     newTitle.push(temp);
